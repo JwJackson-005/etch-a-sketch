d3957db34bfc171137363327c16bff2fcbe8017c
Jason Jackson <jwjackson.wsu@gmail.com>
2021-12-27 23:37:48 -0800

    base functionality added

--- javascript.js
+++ javascript.js
@@ -1,7 +1,9 @@
-let gridSize = 4;
+let gridSize = 100;
 
 function buildGrid() {
     const container = document.querySelector('#gridContainer');
+    const gridLength = container.clientHeight;
+    console.log(gridLength);
 
     for (let i = 0; i < gridSize; i++){
         let row = document.createElement('div');
@@ -9,6 +11,9 @@ function buildGrid() {
         for (let j = 0; j < gridSize; j++) {
             let col = document.createElement('div');
             col.classList.add('col');
+            col.style.height = `${gridLength/gridSize}px`;
+            col.style.width = `${gridLength/gridSize}px`;
+            col.style.backgroundColor = 'white';
             col.addEventListener('mouseenter', color);
             row.appendChild(col);
         }
@@ -21,4 +26,34 @@ function color(e) {
         cell.style.backgroundColor = 'red';
 }
 
-buildGrid();
+function initialize() {
+    const clearButton = document.querySelector('#clearButton');
+    clearButton.addEventListener('click', clearGrid);
+    buildGrid();
+}
+
+function clearGrid() {
+    const cells = document.querySelectorAll('.col');
+
+    for (let cell of cells) {
+        cell.remove();
+    }
+
+    gridSize = getGridSize();
+    buildGrid();
+}
+
+function getGridSize() {
+    let answer = 0;
+
+    do {
+    answer = prompt('Enter a grid size (1-100)');
+    answer = parseInt(answer);
+    } while(!Number.isInteger(answer) || answer < 1 || answer > 100);
+
+    return answer;
+
+}
+
+
+initialize();
